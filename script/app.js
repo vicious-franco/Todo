2ab7d6fab0ee96aab86366bd7541dca4ea8cce40
vicious <irakaramale@gmail.com>
2025-05-08 16:00:48 +0200

    add check

--- script/app.js
+++ script/app.js
@@ -1,26 +1,45 @@
 const todo = document.querySelector(".todo_input");
 const add = document.getElementById("add");
 let todoCounter = document.querySelector(".todoCounter");
-let todoContainer = document.querySelector(".Todo_output");
+let todoContainer = document.querySelector(".list");
 
-const todoList = ["leon"];
+const todoList = [];
 let renderTodo = "";
 const htmlData = (info) => {
   return `
         <div class="text-render">
         <div class="list_p">
-            <div class="circle">
-                <img src="./images/icon-check.svg" alt="" />
+            <div class="circle">   
             </div>
-            <p>${info}</p>
+            <p class="info">${info}</p>
         </div>
-            <img src="./images/icon-cross.svg" alt="" />
+            <img src="./images/icon-cross.svg" alt="cross" />
     </div>`;
 };
-
 add.addEventListener("click", () => {
   const data = todo.value;
   todoList.push(data);
-  todoList.forEach((todo) => htmlData(todo));
-  todoContainer.innerHTML = htmlData();
+  todoContainer.innerHTML = todoList.map((data) => htmlData(data)).join("");
+  todoCounter.innerHTML =
+    todoList.length === 1 ? "1 Item" : `${todoList.length} items`;
+});
+
+document.addEventListener("click", (e) => {
+  const circle = e.target.closest(".circle");
+  const lineThrough = e.target.closest(".list_p");
+  lineThrough.classList.toggle("completed");
+  if (circle) {
+    const checkedIcon = document.createElement("img");
+
+    circle.classList.toggle("circle_check");
+
+    if (circle.classList.contains("circle_check")) {
+      checkedIcon.src = "./images/icon-check.svg";
+      circle.appendChild(checkedIcon);
+    }
+  } else {
+    if (checkedIcon) {
+      circle.removeChild(checkedIcon);
+    }
+  }
 });
